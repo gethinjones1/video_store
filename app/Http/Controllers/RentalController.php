23c03225a626a58084ac42c2db98ae97a6abe6e6
gethinjones1 <gethinjackjones@gmail.com>
2022-05-22 15:26:27 +0100

    update array build
    Make cause performance bottle necking due to multiple db queries.

--- app/Http/Controllers/RentalController.php
+++ app/Http/Controllers/RentalController.php
@@ -25,31 +25,27 @@ class RentalController extends Controller
             return '500'; //send http status
         }
 
+        $rentalString = explode(',', $rentalString); // Initial query string to an array
+        $rentalArray = array_chunk($rentalString, 2); // The input is filmname,duration split the array in chunks
+        $receiptArray = array(); // Initialise new array to push the above and then append the type
 
-        $rentalString = explode(',', $rentalString);
-        $rentalArray = array_chunk($rentalString, 2);
-
-        $rentalFilm = array();
-
-        foreach($rentalFilm as $film) {
-            array_push($rentalFilm ,$film[0]);
+        foreach($rentalArray as $rental) {
+            $movie = Rentals::where('movie_name', $rental[0])->get('movie_type');
+            array_push($receiptArray, $rental[0]);
+            array_push($receiptArray, $rental[1]);
+            array_push($receiptArray, $movie[0]['movie_type']);
         }
 
-        //Take the type
-        $movies = Rentals::whereIn('movie_name', $rentalFilm)->get();
-        
-        //append type to film, duration OR replace the filmname with the movieObject
-        foreach($movies as $movie) {
-            $movie['movie_type'];
-        }
+        // Array will now contain film,duration,type needs to be chunked into own array
+        $receiptArray = array_chunk($receiptArray, 3);
+
 
         //algo
         $receiptClass = new Receipt();
-        $receipt = $receiptClass->create($rentalArray);
+        $receipt = $receiptClass->create($receiptArray);
         
-        return $receipt;
+        return  $receipt;
 
-        //return $rentalString;
     }
 
 }
--- app/Utility/Receipt.php
+++ app/Utility/Receipt.php
@@ -12,7 +12,8 @@ class Receipt {
         foreach($rentalArray as $rental) {
             $film = $rental[0]; //film.name AND film.type 
             $duration = $rental[1];
-            $statementLine = $this->lineItem($film, $duration);
+            $type = $rental[2];
+            $statementLine = $this->lineItem($film, $duration, $type);
             array_push($rentalResult, $statementLine);
             //Increment price and points
         }
@@ -20,27 +21,27 @@ class Receipt {
         return $rentalResult;
     }
 
-    public function lineItem($film, $duration) {
+    public function lineItem($film, $duration, $type) {
         $price = 0;
         $points = 0;
         $NEW_FILM_RATE = 3;
       
-        if ($film->movie_type == "new") {
+        if ($type == "new") {
           $price = $NEW_FILM_RATE * $duration;
           $points = $duration < 2 ? 1 : 2;
         }
       
-        if ($film->movie_type == "regular") {
+        if ($type == "regular") {
           $price = 2 + max($duration - 2, 0) * 1.5;
           $points = 1;
         }
       
-        if ($film->movie_type == "childrens") {
+        if ($type == "childrens") {
           $price = 1.5 + max($duration - 3, 0) * 1.5;
           $points = 1;
         }
 
-        return array("price"=>$price, "points"=>$points, "filmName"=>$film->movie_name, "duration"=>$duration);
+        return array("price"=>$price, "points"=>$points, "filmName"=>$film, "duration"=>$duration);
 
     }
 }
